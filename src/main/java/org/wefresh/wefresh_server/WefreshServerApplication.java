bca96b2a3330190e55676b8ed901069b7ec15479
chaewonkim <kimlove9182@gmail.com>
2025-02-21 17:48:41 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/org/wefresh/wefresh_server/WefreshServerApplication.java
@@ -0,0 +1,13 @@
+package org.wefresh.wefresh_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefreshServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WefreshServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wefresh/wefresh_server/WefreshServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wefresh.wefresh_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefreshServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
