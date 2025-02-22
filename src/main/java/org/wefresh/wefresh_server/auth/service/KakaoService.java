dd64222487568eb8991e0bc4b7b0ef733f241f9e
chaewonkim <kimlove9182@gmail.com>
2025-02-23 05:37:02 +0900

    feat: #13 kakao 토큰 발급, 유저 정보 조회 로직 구현

---
+++ src/main/java/org/wefresh/wefresh_server/auth/service/KakaoService.java
@@ -0,0 +1,44 @@
+package org.wefresh.wefresh_server.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.wefresh.wefresh_server.auth.dto.SocialUserDto;
+import org.wefresh.wefresh_server.common.auth.constant.AuthConstant;
+import org.wefresh.wefresh_server.external.service.kakao.KakaoFeignClient;
+import org.wefresh.wefresh_server.external.service.kakao.KakaoTokenClient;
+import org.wefresh.wefresh_server.external.service.kakao.dto.KakaoTokenDto;
+import org.wefresh.wefresh_server.external.service.kakao.dto.KakaoUserDto;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoService {
+
+    private final KakaoTokenClient kakaoTokenClient;
+    private final KakaoFeignClient kakaoFeignClient;
+
+    @Value("${oauth.kakao.client-id}")
+    private String clientId;
+
+    @Value("${oauth.kakao.redirect-uri}")
+    private String redirectUri;
+
+    public KakaoTokenDto getSocialToken(
+            final String code
+    ) {
+        return kakaoTokenClient.getToken(
+                code,
+                clientId,
+                redirectUri,
+                "authorization_code"
+        );
+    }
+
+    public SocialUserDto getSocialUserInfo(final String providerToken) {
+        KakaoUserDto kakaoUserDto = kakaoFeignClient.getUserInformation(AuthConstant.BEARER_TOKEN_PREFIX + providerToken);
+        return SocialUserDto.of(
+                kakaoUserDto.id().toString(),
+                kakaoUserDto.kakaoAccount().email(),
+                kakaoUserDto.kakaoAccount().profile().profileImageUrl());
+    }
+}
