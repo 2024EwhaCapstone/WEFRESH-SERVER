98f2a8dc5cb0c6cb68e7c5c7208b6b1b4004d956
chaewonkim <kimlove9182@gmail.com>
2025-02-22 22:44:43 +0900

    feat: #7 서비스 생성

---
+++ src/main/java/org/wefresh/wefresh_server/food/service/FoodService.java
@@ -0,0 +1,9 @@
+package org.wefresh.wefresh_server.food.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FoodService {
+}
