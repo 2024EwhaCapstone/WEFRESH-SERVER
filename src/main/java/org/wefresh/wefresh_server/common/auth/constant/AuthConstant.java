0f2a6299c80589a5b99fa16d4a59e69315f86645
chaewonkim <kimlove9182@gmail.com>
2025-02-23 05:38:20 +0900

    feat: #13 인증 관련 상수, 에러코드 생성

--- src/main/java/org/wefresh/wefresh_server/common/auth/constant/AuthConstant.java
+++ src/main/java/org/wefresh/wefresh_server/common/auth/constant/AuthConstant.java
@@ -10,4 +10,9 @@ public class AuthConstant {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_TOKEN_PREFIX = "Bearer ";
     public static final String CHARACTER_ENCODING_UTF8 = "utf-8";
+
+    // KAKAO
+    public static final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+    public static final String KAKAO_URL = "https://kapi.kakao.com";
+
 }
