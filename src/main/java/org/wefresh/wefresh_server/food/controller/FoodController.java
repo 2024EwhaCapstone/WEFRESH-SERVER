054ef43d2a30edc9fc9ebbfaca896f793576d9ac
chaewonkim <kimlove9182@gmail.com>
2025-02-22 22:44:20 +0900

    feat: #7 컨트롤러 생성

---
+++ src/main/java/org/wefresh/wefresh_server/food/controller/FoodController.java
@@ -0,0 +1,9 @@
+package org.wefresh.wefresh_server.food.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FoodController {
+}
