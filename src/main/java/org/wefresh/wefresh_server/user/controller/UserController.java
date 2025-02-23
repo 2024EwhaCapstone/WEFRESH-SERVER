01073c4aecec50824906768c33eae637519e8d53
chaewonkim <kimlove9182@gmail.com>
2025-02-24 00:19:02 +0900

    feat: #21 로그인한 유저 정보 조회 구현

--- src/main/java/org/wefresh/wefresh_server/user/controller/UserController.java
+++ src/main/java/org/wefresh/wefresh_server/user/controller/UserController.java
@@ -1,9 +1,24 @@
 package org.wefresh.wefresh_server.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wefresh.wefresh_server.common.auth.annotation.UserId;
+import org.wefresh.wefresh_server.common.dto.ResponseDto;
+import org.wefresh.wefresh_server.user.dto.response.UserDto;
+import org.wefresh.wefresh_server.user.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/me")
+    public ResponseEntity<ResponseDto<UserDto>> getUserInfo(
+            @UserId final Long userId
+    ) {
+        return ResponseEntity.ok().body(ResponseDto.success(userService.getUserInfo(userId)));
+    }
 }
--- src/main/java/org/wefresh/wefresh_server/user/service/UserService.java
+++ src/main/java/org/wefresh/wefresh_server/user/service/UserService.java
@@ -2,8 +2,20 @@ package org.wefresh.wefresh_server.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.wefresh.wefresh_server.user.domain.User;
+import org.wefresh.wefresh_server.user.dto.response.UserDto;
+import org.wefresh.wefresh_server.user.manager.UserRetriever;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRetriever userRetriever;
+
+    @Transactional(readOnly = true)
+    public UserDto getUserInfo(final Long userId) {
+        User user = userRetriever.findById(userId);
+        return UserDto.from(user);
+    }
 }
