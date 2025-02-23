ed59560ecc732135909710fbc9cb6e22898a1ddb
chaewonkim <kimlove9182@gmail.com>
2025-02-24 00:41:05 +0900

    fix: #23 BaseTimeEntity 작동하도록 수정

--- src/main/java/org/wefresh/wefresh_server/WefreshServerApplication.java
+++ src/main/java/org/wefresh/wefresh_server/WefreshServerApplication.java
@@ -2,8 +2,10 @@ package org.wefresh.wefresh_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WefreshServerApplication {
 
 	public static void main(String[] args) {
