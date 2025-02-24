d6560e6e26efa1fd3fc047726056781c7f033dad
chaewonkim <kimlove9182@gmail.com>
2025-02-24 18:13:57 +0900

    feat: #25 음식 등록 로직 구현

--- src/main/java/org/wefresh/wefresh_server/food/controller/FoodController.java
+++ src/main/java/org/wefresh/wefresh_server/food/controller/FoodController.java
@@ -1,9 +1,27 @@
 package org.wefresh.wefresh_server.food.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wefresh.wefresh_server.common.auth.annotation.UserId;
+import org.wefresh.wefresh_server.common.dto.ResponseDto;
+import org.wefresh.wefresh_server.food.dto.request.FoodRegisterDto;
+import org.wefresh.wefresh_server.food.service.FoodService;
 
 @RestController
 @RequiredArgsConstructor
 public class FoodController {
+
+    private final FoodService foodService;
+
+    @PostMapping("/foods")
+    public ResponseEntity<ResponseDto<Void>> registerFood(
+            @UserId final Long userId,
+            @ModelAttribute final FoodRegisterDto foodRegisterDto
+    ) {
+        foodService.registerFood(userId, foodRegisterDto);
+        return ResponseEntity.ok().body(ResponseDto.success());
+    }
 }
