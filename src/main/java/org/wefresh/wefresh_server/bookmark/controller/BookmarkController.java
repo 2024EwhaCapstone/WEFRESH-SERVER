f3afbac372bd465eca4e0177f4c4d4b736680030
chaewonkim <kimlove9182@gmail.com>
2025-02-25 02:23:28 +0900

    feat: #35 추천 레시피 저장 로직 구현

--- src/main/java/org/wefresh/wefresh_server/bookmark/controller/BookmarkController.java
+++ src/main/java/org/wefresh/wefresh_server/bookmark/controller/BookmarkController.java
@@ -1,9 +1,25 @@
 package org.wefresh.wefresh_server.bookmark.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.wefresh.wefresh_server.bookmark.service.BookmarkService;
+import org.wefresh.wefresh_server.common.auth.annotation.UserId;
+import org.wefresh.wefresh_server.common.dto.ResponseDto;
 
 @RestController
 @RequiredArgsConstructor
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("/recipes/{id}")
+    public ResponseEntity<ResponseDto<Void>> createBookmark(
+            @UserId final Long userId,
+            @PathVariable final Long id,
+            @RequestParam(required = false) final String type
+    ) {
+        bookmarkService.createBookmark(userId, id, type);
+        return ResponseEntity.ok().body(ResponseDto.success());
+    }
 }
--- src/main/java/org/wefresh/wefresh_server/bookmark/service/BookmarkService.java
+++ src/main/java/org/wefresh/wefresh_server/bookmark/service/BookmarkService.java
@@ -2,8 +2,64 @@ package org.wefresh.wefresh_server.bookmark.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.wefresh.wefresh_server.bookmark.domain.Bookmark;
+import org.wefresh.wefresh_server.bookmark.manager.BookmarkCreator;
+import org.wefresh.wefresh_server.common.exception.BusinessException;
+import org.wefresh.wefresh_server.common.exception.code.RecipeErrorCode;
+import org.wefresh.wefresh_server.recipe.domain.Recipe;
+import org.wefresh.wefresh_server.recipe.domain.RecipeBase;
+import org.wefresh.wefresh_server.recipe.manager.RecipeRetriever;
+import org.wefresh.wefresh_server.todayRecipe.domain.TodayRecipe;
+import org.wefresh.wefresh_server.todayRecipe.manager.TodayRecipeRetriever;
+import org.wefresh.wefresh_server.user.domain.User;
+import org.wefresh.wefresh_server.user.manager.UserRetriever;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
+
+    private final BookmarkCreator bookmarkCreator;
+    private final UserRetriever userRetriever;
+    private final RecipeRetriever recipeRetriever;
+    private final TodayRecipeRetriever todayRecipeRetriever;
+
+    @Transactional
+    public void createBookmark(
+            final Long userId,
+            final Long id,
+            final String type
+    ) {
+        User user = userRetriever.findById(userId);
+
+        Bookmark bookmark = null;
+        if (type.equals("general")) {
+            Recipe recipe = recipeRetriever.findById(id);
+            bookmark = buildBookmark(recipe, user);
+        } else if (type.equals("today")) {
+            TodayRecipe todayRecipe = todayRecipeRetriever.findById(id);
+            bookmark = buildBookmark(todayRecipe, user);
+        } else {
+            throw new BusinessException(RecipeErrorCode.RECIPE_NOT_FOUND);
+        }
+
+        bookmarkCreator.save(bookmark);
+    }
+
+    private Bookmark buildBookmark(RecipeBase recipe, User user) {
+        if (recipe instanceof Recipe generalRecipe) {
+            return Bookmark.builder()
+                    .recipe(generalRecipe)
+                    .user(user)
+                    .build();
+        } else if (recipe instanceof TodayRecipe todayRecipe) {
+            return Bookmark.builder()
+                    .todayRecipe(todayRecipe)
+                    .user(user)
+                    .build();
+        } else {
+            throw new BusinessException(RecipeErrorCode.RECIPE_NOT_FOUND);
+        }
+    }
+
 }
