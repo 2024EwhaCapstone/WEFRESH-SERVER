a4dad348fd57e6de236d889193166b511e719f21
chaewonkim <kimlove9182@gmail.com>
2025-04-06 02:01:02 +0900

    fix: 오늘의 레시피는 하루에 한번만 호출되도록 수정

--- src/main/java/org/wefresh/wefresh_server/todayRecipe/repository/TodayRecipeRepository.java
+++ src/main/java/org/wefresh/wefresh_server/todayRecipe/repository/TodayRecipeRepository.java
@@ -3,7 +3,13 @@ package org.wefresh.wefresh_server.todayRecipe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.wefresh.wefresh_server.todayRecipe.domain.TodayRecipe;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TodayRecipeRepository extends JpaRepository<TodayRecipe, Long> {
 
     void deleteByUserId(Long id);
+
+    List<TodayRecipe> findAllByUserIdAndRecommendedDate(Long userId, LocalDate date);
+
 }
